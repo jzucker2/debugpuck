362f37bd5218b813eb56cabb6e18a1c6979e7811
Jordan Zucker <jordan.zucker@gmail.com>
2015-02-14 15:43:29 -0800

    Initial Commit

---
+++ debugpuck/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  debugpuck
+//
+//  Created by Jordan Zucker on 2/14/15.
+//  Copyright (c) 2015 Jordan Zucker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ debugpuck/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  debugpuck
+//
+//  Created by Jordan Zucker on 2/14/15.
+//  Copyright (c) 2015 Jordan Zucker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
